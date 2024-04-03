acdf7b8138350ccab5f77ae1d32c4d23a3e0eda2
edelbarno <barnoedel12@gmail.com>
2024-04-03 21:01:55 +0300

    add first class functions

---
+++ index.js
@@ -0,0 +1,25 @@
+
+function receivesAFunction(callback){
+    console.log(spy)
+}
+    return receivesAFunction;
+  
+ 
+function returnsANamedFunction() {
+ function namedFunction(){
+    console.log('a named function')
+ }
+ return namedFunction;
+    }
+
+
+function returnsAnAnonymousFunction(){
+    function anonymousFunction(){
+console.log ('before all')
+    }
+    return namedFunction()
+        
+    };
+
+const myAnonymousFunction = returnsAnAnonymousFunction();
+myAnonymousFunction();
